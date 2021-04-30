0a9a4b8d646de27440abc1ef5274e96d23db620e
jacobbra <Jacobbra@kth.se>
2021-04-30 23:38:28 +0200

    Interdiamate push

---
+++ 05/bintree.cpp
@@ -0,0 +1,39 @@
+#include"bintree.h"
+
+void insert(Node * & p, int key, double to_be_inserted)
+{
+
+}
+void remove(Node * & p, const int & key)
+{
+    
+}
+const double & find(Node * p, const int & to_be_found)
+{
+    
+}
+double & edit(Node * p, const int & to_be_changed)
+{
+    
+}
+void delete_tree(Node * & p)
+{
+    
+}
+
+unsigned int max_height(Node * p)
+{
+    
+}
+unsigned int min_height(Node * p)
+{
+    
+}
+unsigned int size(Node * p)
+{
+    
+}
+bool is_balanced(Node * p)
+{
+    
+}
---
+++ 05/testtree.h
@@ -0,0 +1,12 @@
+#include<cxxtest/TestSuite.h>
+#include"bintree.h"
+#include<fstream>
+
+class MyTestSuite : public CxxTest::TestSuite 
+{
+public:
+
+
+private:
+
+};
