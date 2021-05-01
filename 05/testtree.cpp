a7407d9725d05877cb9f1fc0a96c758af4025c1e
jackohass <jacob.brannstrom@telia.com>
2021-05-01 14:06:36 +0200

    Add tests for find

--- 05/testtree.cpp
+++ 05/testtree.cpp
@@ -5,6 +5,7 @@
 #endif
 
 #define _CXXTEST_HAVE_STD
+#define _CXXTEST_HAVE_EH
 #include <cxxtest/TestListener.h>
 #include <cxxtest/TestTracker.h>
 #include <cxxtest/TestRunner.h>
@@ -25,31 +26,61 @@ bool suite_MyTestSuite_init = false;
 static MyTestSuite suite_MyTestSuite;
 
 static CxxTest::List Tests_MyTestSuite = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "testtree.h", 5, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
+CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "testtree.h", 7, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
 
 static class TestDescription_suite_MyTestSuite_testInsert0 : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_testInsert0() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 8, "testInsert0" ) {}
+ TestDescription_suite_MyTestSuite_testInsert0() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 10, "testInsert0" ) {}
  void runTest() { suite_MyTestSuite.testInsert0(); }
 } testDescription_suite_MyTestSuite_testInsert0;
 
 static class TestDescription_suite_MyTestSuite_testInsert1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_testInsert1() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 18, "testInsert1" ) {}
+ TestDescription_suite_MyTestSuite_testInsert1() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 20, "testInsert1" ) {}
  void runTest() { suite_MyTestSuite.testInsert1(); }
 } testDescription_suite_MyTestSuite_testInsert1;
 
 static class TestDescription_suite_MyTestSuite_testInsert1Overwrite : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_testInsert1Overwrite() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 31, "testInsert1Overwrite" ) {}
+ TestDescription_suite_MyTestSuite_testInsert1Overwrite() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 33, "testInsert1Overwrite" ) {}
  void runTest() { suite_MyTestSuite.testInsert1Overwrite(); }
 } testDescription_suite_MyTestSuite_testInsert1Overwrite;
 
 static class TestDescription_suite_MyTestSuite_testInsertMore : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_testInsertMore() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 45, "testInsertMore" ) {}
+ TestDescription_suite_MyTestSuite_testInsertMore() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 47, "testInsertMore" ) {}
  void runTest() { suite_MyTestSuite.testInsertMore(); }
 } testDescription_suite_MyTestSuite_testInsertMore;
+
+static class TestDescription_suite_MyTestSuite_testFind0 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testFind0() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 66, "testFind0" ) {}
+ void runTest() { suite_MyTestSuite.testFind0(); }
+} testDescription_suite_MyTestSuite_testFind0;
+
+static class TestDescription_suite_MyTestSuite_testFindRoot : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testFindRoot() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 71, "testFindRoot" ) {}
+ void runTest() { suite_MyTestSuite.testFindRoot(); }
+} testDescription_suite_MyTestSuite_testFindRoot;
+
+static class TestDescription_suite_MyTestSuite_testFindMiddle : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testFindMiddle() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 84, "testFindMiddle" ) {}
+ void runTest() { suite_MyTestSuite.testFindMiddle(); }
+} testDescription_suite_MyTestSuite_testFindMiddle;
+
+static class TestDescription_suite_MyTestSuite_testFindLeaf : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testFindLeaf() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 97, "testFindLeaf" ) {}
+ void runTest() { suite_MyTestSuite.testFindLeaf(); }
+} testDescription_suite_MyTestSuite_testFindLeaf;
+
+static class TestDescription_suite_MyTestSuite_testFindNothing : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testFindNothing() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 110, "testFindNothing" ) {}
+ void runTest() { suite_MyTestSuite.testFindNothing(); }
+} testDescription_suite_MyTestSuite_testFindNothing;
 
 #include <cxxtest/Root.cpp>
 const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
--- 05/testtree.h
+++ 05/testtree.h
@@ -1,6 +1,8 @@
 #include<cxxtest/TestSuite.h>
 #include"bintree.h"
-#include<fstream>
+#include<stdexcept>
+
+using namespace std;
 
 class MyTestSuite : public CxxTest::TestSuite 
 {
@@ -59,6 +61,64 @@ public:
         TS_ASSERT(temp->data == 22);
         TS_ASSERT(temp->left == nullptr);
         TS_ASSERT(temp->right == nullptr);
+    }
+
+    void testFind0( void )
+    {
+        Node* data = nullptr;
+        TS_ASSERT_THROWS(find(data, 0), out_of_range);
+    }
+    void testFindRoot( void )
+    {
+        Node* data = nullptr;
+        insert(data,0,12);
+        insert(data,2,22);
+        insert(data,-2,32);
+        insert(data,1,42);
+        insert(data,3,52);
+        insert(data,-1,62);
+        insert(data,-3,72);
+
+        TS_ASSERT(find(data,0) == 12);
+    }
+    void testFindMiddle( void )
+    {
+        Node* data = nullptr;
+        insert(data,0,12);
+        insert(data,2,22);
+        insert(data,-2,32);
+        insert(data,1,42);
+        insert(data,3,52);
+        insert(data,-1,62);
+        insert(data,-3,72);
+
+        TS_ASSERT(find(data,2) == 22);
+    }
+    void testFindLeaf( void )
+    {
+        Node* data = nullptr;
+        insert(data,0,12);
+        insert(data,2,22);
+        insert(data,-2,32);
+        insert(data,1,42);
+        insert(data,3,52);
+        insert(data,-1,62);
+        insert(data,-3,72);
+
+        TS_ASSERT(find(data,3) == 52);
+    }
+    void testFindNothing( void )
+    {
+        Node* data = nullptr;
+        insert(data,0,12);
+        insert(data,2,22);
+        insert(data,-2,32);
+        insert(data,1,42);
+        insert(data,3,52);
+        insert(data,-1,62);
+        insert(data,-3,72);
+
+        TS_ASSERT_THROWS(find(data, 32), out_of_range);
     }
 
 private:
