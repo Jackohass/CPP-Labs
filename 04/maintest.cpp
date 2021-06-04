db259e7dd4febf17dc1bb8484ac0b2a4f3a823d1
jackohass <jacob.brannstrom@telia.com>
2021-06-04 20:44:59 +0200

    Tried at komplettering

--- 04/maintest.cpp
+++ 04/maintest.cpp
@@ -7,20 +7,57 @@ using namespace std;
 void testBoards()
 {
     ChessBoard chess;
+    int numWCM, numBCM, numWNCM, numBNCM;
     ifstream myfile("data.txt");
     if (myfile.is_open())
     {
+        int numPassed = 0;
+        int numTests = 0;
         while (!myfile.eof())
         {
+            bool passed = true;
             myfile >> chess;
+            myfile >> numWCM;
+            myfile >> numBCM;
+            myfile >> numWNCM;
+            myfile >> numBNCM;
             cout << chess;
             vector<ChessMove> vW = chess.capturingMoves(true);
-            cout << "Number of white capturing moves: " << vW.size() << " moves\n";
             vector<ChessMove> vB = chess.capturingMoves(false);
-            cout << "Number of black capturing moves: " << vB.size() << " moves\n";
+            vector<ChessMove> vnW = chess.nonCapturingMoves(true);
+            vector<ChessMove> vnB = chess.nonCapturingMoves(false);
+            if (vW.size() != numWCM) {
+                cout << "capturingMoves white FAILED, expected " << numWCM << " moves but got " << vW.size() << " moves" << endl;
+                passed = false;
+            } else {
+                cout << "capturingMoves white PASSED, expected " << numWCM << " moves and got " << vW.size() << " moves" << endl;
+            }
+            if (vB.size() != numBCM) {
+                cout << "capturingMoves black FAILED, expected " << numBCM << " moves but got " << vB.size() << " moves" << endl;
+                passed = false;
+            } else {
+                cout << "capturingMoves black PASSED, expected " << numBCM << " moves and got " << vB.size() << " moves" << endl;
+            }
+
+            if (vnW.size() != numWNCM) {
+                cout << "nonCapturingMoves white FAILED, expected " << numWNCM << " moves but got " << vnW.size() << " moves" << endl;
+                passed = false;
+            } else {
+                cout << "nonCapturingMoves white PASSED, expected " << numWNCM << " moves and got " << vnW.size() << " moves" << endl;
+            }
+            if (vnB.size() != numBNCM) {
+                cout << "nonCapturingMoves black FAILED, expected " << numBNCM << " moves but got " << vnB.size() << " moves" << endl;
+                passed = false;
+            } else {
+                cout << "nonCapturingMoves black PASSED, expected " << numBNCM << " moves and got " << vnB.size() << " moves" << endl;
+            }
             cout << endl;
+            numTests++;
+            if(passed) numPassed++;
         }
         myfile.close();
+
+        cout << numPassed << "/" << numTests << " passed" << endl;
     }
 }
 
@@ -107,6 +144,22 @@ int main()
     // s << "........" << endl;
     // s << "........" << endl;
     // s << "PPPPPPPP" << endl;
+    //s << "RNBQKBNR";
+    // s << "........" << endl;
+    // s << "........" << endl;
+    // s << "........" << endl;
+    // s << ".r......" << endl;
+    // s << ".p......" << endl;
+    // s << "...P...n" << endl;
+    // s << "......P." << endl;
+    // s << ".N...B..";
+    // s << "rnbqkbnr" << endl;
+    // s << "pppppppp" << endl;
+    // s << "........" << endl;
+    // s << "........" << endl;
+    // s << "....P..." << endl;
+    // s << "........" << endl;
+    // s << "PPPP.PPP" << endl;
     // s << "RNBQKBNR";
 
     // cout << s.str() << endl;
@@ -118,10 +171,10 @@ int main()
     // cout << chess << endl;
     // cout << chess.getPiece(7,7)->print(false).compare("p") << endl;
 
-    // vector<ChessMove> v = chess.capturingMoves(false);
+    // vector<ChessMove> v = chess.capturingMoves(true);
     // cout << "capturingMoves: got " << v.size() << " moves" << endl;
     // cout << "capturingMoves: " << v << endl;
-    // vector<ChessMove> v0 = chess.nonCapturingMoves(false);
+    // vector<ChessMove> v0 = chess.nonCapturingMoves(true);
     // cout << "nonCapturingMoves: got " << v0.size() << " moves" << endl;
     // cout << "nonCapturingMoves: " << v0 << endl;
 
